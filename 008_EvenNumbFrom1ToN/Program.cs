835791ee033d524b69165ff40e0b60c29378cd83
Alex <aov2orom@yandex.ru>
2022-10-07 00:25:07 +0300

    Ex008 Четные числа от 1 до N

---
+++ 008_EvenNumbFrom1ToN/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/ 
+
+/*
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while(1 <= n)
+{
+    if (n % 2 == 0)
+    {
+        Console.Write(n + " ");
+        n--;
+    }
+    else n--;
+}
+*/
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+
+while (a <= n)
+{
+    if (a % 2 == 0)
+    {
+        Console.Write(a + " ");
+        a++;
+    }
+    else a++;
+}
