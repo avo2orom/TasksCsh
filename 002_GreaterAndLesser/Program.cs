b59de1e472d7395cb0178ff8d8b8fc7335f68689
Alex <aov2orom@yandex.ru>
2022-10-06 22:54:45 +0300

    Add Ex 002 Найти большее и меньшее из двух

--- 002_GreaterAndLesser/Program.cs
+++ 002_GreaterAndLesser/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+
+Console.WriteLine("Введите число 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine("Число 1 больше числа 2");
+}
+else 
+{
+    Console.WriteLine("Число 2 больше числа 1");
+}
