b8f8422015dd9c4b364f8cf7377417a06c26459e
Alex <aov2orom@yandex.ru>
2022-10-10 21:48:53 +0300

    Add Ex 013 Third number

---
+++ 013_ThirdNum/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.WriteLine("Введите любое число");
+
+try
+{
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2){
+  Console.WriteLine("третья цифра " + NumberText[2]);
+}
+else {
+  Console.WriteLine("третьей цифры нет");
+}
+}
+catch (FormatException)
+{
+    Console.WriteLine("Неверный формат");
+}
