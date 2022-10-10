6c15bbec56db20caef83e71934ca4ff84fde2acc
Alex <aov2orom@yandex.ru>
2022-10-10 21:22:27 +0300

    Add Ex 010 Second num of tree

---
+++ 010_SecondNumOfThree'/Program.cs
@@ -0,0 +1,48 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 
+
+Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 99 | n > 999)
+{
+    Console.WriteLine("Введено некорректное число");
+    return;
+}
+
+else
+{
+    int n1 = n % 100;
+    int n2 = n1 / 10;
+    
+    Console.WriteLine(n2);
+}
+*/
+
+Console.WriteLine("Введите трехзначное число");
+
+try {
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 99 | n > 999)
+{
+    Console.WriteLine("Введено некорректное число");
+    return;
+}
+
+else
+{
+    int n1 = n % 100;
+    int n2 = n1 / 10;
+    
+    Console.WriteLine(n2);
+}
+} 
+catch (FormatException)
+{
+Console.WriteLine("Неверный формат");
+}
