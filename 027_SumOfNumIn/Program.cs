32f960f6e992b103d8c29baf39ec39e77de8e775
Alex <aov2orom@yandex.ru>
2022-10-15 19:00:58 +0300

    Add 027

---
+++ 027_SumOfNumIn/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/ 
+
+Console.Write("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+string numberNtext= Convert.ToString(numberN);
+
+  int SumNumber(int numberN)
+  {
+    int a = 0;
+    int result = 0;
+
+    for (int i = 0; i < numberNtext.Length; i++){
+      a = numberN - numberN % 10;
+      result = result + (numberN - a);
+      numberN = numberN / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(numberN);
+Console.WriteLine(sumNumber);
