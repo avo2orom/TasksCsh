dba51c367a0e9a60fc72b1e02c626edf8488f31a
Alex <aov2orom@yandex.ru>
2022-10-19 22:12:39 +0300

    ADD 035

---
+++ 035_HowManyNumInLine/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+[5, 18, 123, 6, 2] -> 1
+[1, 2, 3, 6, 2] -> 0
+[10, 11, 12, 13, 14] -> 5
+
+Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21
+*/
+
+int[] arr = new int[123];
+
+void newArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 300);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < (array.Length - 1)) Console.Write($", ");
+
+    }
+    Console.WriteLine();
+}
+
+void CountNumInLine(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] >= 10 && array [i] <=100)
+            count = count + 1;
+    }
+    Console.Write(count);
+}
+
+Console.Write("Массив: ");
+newArray(arr);
+PrintArray(arr);
+Console.Write("Количество элементов массива в отрезке от 10 до 99 составляет: ");
+CountNumInLine(arr);
