fe6f570e91caf987c3ccf405681540d90b200bb6
Alex <aov2orom@yandex.ru>
2022-10-20 22:35:06 +0300

    Add 036

---
+++ 036_SumOfElementsInoddPosit/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 36: 
+Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+
+*/
+
+int[] arr = new int[8];
+
+void newArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < (array.Length - 1)) Console.Write($", ");
+
+    }
+    Console.WriteLine();
+}
+
+void SumOfElementsInOddPositions(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+
+    }
+    Console.WriteLine(sum);
+}
+
+
+Console.Write("Массив: ");
+newArray(arr);
+PrintArray(arr);
+Console.Write("Сумма элементов, стоящих на нечётных позициях: ");
+SumOfElementsInOddPositions(arr);
