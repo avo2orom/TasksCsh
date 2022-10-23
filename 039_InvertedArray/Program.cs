a4b147a2b1f1ff74afc474b787dd05d34a3db0da
Alex <aov2orom@yandex.ru>
2022-10-23 12:22:18 +0300

    Add 039 inverted array

---
+++ 039_InvertedArray/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] arr = new int[8];
+int[] arr2 = new int[8];
+
+void newArray(int[] array)
+{   
+    Console.Write("Массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+        Console.Write(array[i] + " ");
+    }
+}
+
+void InvertedArray(int[] array)
+{
+    for (int i = 0; i < (array.Length) / 2; i++)
+    {
+        int temporary = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temporary;
+    }
+    Console.WriteLine();
+    Console.Write("Перевернутый массив: ");
+    for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+}
+
+newArray(arr);
+InvertedArray(arr);
