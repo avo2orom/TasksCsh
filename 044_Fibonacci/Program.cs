f0d018f96aa16b01b476575c11705ecf1038b548
Alex <aov2orom@yandex.ru>
2022-10-23 14:14:20 +0300

    Add 044

---
+++ 044_Fibonacci/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+
+*/ 
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("0 1 ");
+void Fibbonacci(int number)
+{
+    int number1 = 0;
+    int number2 = 1;
+    int next = 0;
+    for (int i = 0; i < number; i++)
+    {
+        next = number1 + number2;
+        Console.Write(next + " ");
+        number1 = number2;
+        number2 = next;
+    }
+}
+
+Fibbonacci(number);
